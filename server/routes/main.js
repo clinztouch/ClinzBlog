e406c7e7f4a74975b553b2bd95f372f0e04cfc67
{Calvin Iordye <iordyecaln@gmail.com>
2025-11-15 10:37:22 +0100

    Initial commit

--- server/routes/main.js
+++ server/routes/main.js
@@ -22,7 +22,7 @@ const data = await Post.aggregate([ { $sort: { createdAt: -1 } } ] )
 .limit(perPage)
 .exec();
 
-const count = await Post.countDocuments();
+const count = await Post.countDocuments({});
 const nextPage = parseInt(page) + 1;
 const hasNextPage = nextPage <= Math.ceil(count / perPage);
 
@@ -73,7 +73,11 @@ router.get('/post/:id', async (req, res) => {
        currentRoute: `/post/${slug}`
     }
   
-    res.render('post', { locals, data });
+    res.render('post', { 
+      locals, 
+      data,
+      currentRoute: `/post/${slug}`
+    });
   } catch (error) {
     console.log(error);
   }
@@ -105,7 +109,8 @@ router.post('/search', async (req, res) => {
 
     res.render("search", {
       data,
-      locals
+      locals,
+      currentRoute: '/'
     });
  
   } catch (error) {
@@ -133,7 +138,9 @@ router.get('/about',(req, res) =>{
       title: "Contact",
       description: "Get in touch with us"
     };
-    res.render('contact', { locals, layout: './layouts/main',
+    res.render('contact', { 
+      locals, 
+      layout: './layouts/main',
        currentRoute: '/contact'
      });
   });
@@ -157,49 +164,4 @@ router.get('/about',(req, res) =>{
 
 
 
-  // function insertPostData () {
-  //   Post.insertMany([
-  //     {
-  //       title: "Building APIs with Node.js",
-  //       body: "Learn how to use Node.js to build RESTful APIs using frameworks like Express.js"
-  //     },
-  //     {
-  //           title: "Deployment of Node.js applications",
-  //           body: "Understand the different ways to deploy your Node.js applications, including on-premises, cloud, and container environments..."
-  //        },
-  //        {
-  //         title: "Authentication and Authorization in Node.js",
-  //        body: "Learn how to add authentication and authorization to your Node.js web applications using Passport.js or other authentication libraries."
-  //         },
-  //         {
-  //          title: "Understand how to work with MongoDB and Mongoose",
-  //            body: "Understand how to work with MongoDB and Mongoose, an Object Data Modeling (ODM) library, in Node.js applications."
-  //       },
-  //        {
-  //          title: "build real-time, event-driven applications in Node.js",
-  //         body: "Socket.io: Learn how to use Socket.io to build real-time, event-driven applications in Node.js."
-  //         },
-  //        {
-  //          title: "Discover how to use Express.js",
-  //          body: "Discover how to use Express.js, a popular Node.js web framework, to build web applications."
-  //         },
-  //          {
-  //          title: "Asynchronous Programming with Node.js",
-  //           body: "Asynchronous Programming with Node.js: Explore the asynchronous nature of Node.js and how it allows for non-blocking I/O operations."
-  //          },
-  //         {
-  //           title: "Learn the basics of Node.js and its architecture",
-  //            body: "Learn the basics of Node.js and its architecture, how it works, and why it is popular among developers."
-  //       },
-  //      {
-  //            title: "NodeJs Limiting Network Traffic",
-  //          body: "Learn how to limit netowrk traffic."
-  //        },
-  //         {
-  //           title: "Learn Morgan - HTTP Request logger for NodeJs",
-  //          body: "Learn Morgan."
-  //       },
-  //   ])
-  // }
-  // insertPostData()
-  
+ 
